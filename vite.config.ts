e7a7d87749516cd6b047e30dc05a6fd19bd817f7
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-06-22 10:22:13 +0200

    build: :building_construction: added chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+/**
+ * @type {import('vite').UserConfig}
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { dependencies } from './package.json'
+
+// Packages we want in the vendor aka the deps needed in the entire app.
+const globalVendorPackages = ['react', 'react-dom']
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {}
+  Object.keys(deps).forEach((key) => {
+    if (globalVendorPackages.includes(key)) return
+    chunks[key] = [key]
+  })
+  return chunks
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: globalVendorPackages,
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 })
