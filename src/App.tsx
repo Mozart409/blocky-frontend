257b39db50f98c64e6069682a6fc35cae0fd7300
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-06-22 10:23:11 +0200

    feat: :children_crossing: added darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { BlockingStatus } from './components/BlockingStatus'
 import { RefreshList } from './components/RefreshList'
 import { DNSQuery } from './components/DNSQuery'
 import { Spacer } from './components/Spacer'
+import { ThemeToggle } from './components/ThemeToggle'
+
 function App() {
   return (
     <>
-      <main>
+      <main className="">
         <BlockingStatus />
+        <ThemeToggle />
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="px-4 py-8 sm:px-0">
             <div className="max-w-3xl mx-auto">
